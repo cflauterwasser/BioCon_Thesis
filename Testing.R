247a212f74712622f1c749e258b94d90960c45bb
cflauterwasser <c.f.lauterwasser@posteo.de>
2025-01-09 15:05:09 +0100

    recent additions

--- Testing.R
+++ Testing.R
@@ -1,5 +1,5 @@
-#_______________________________________________________________________
-####  Loading Packages ####
+#___________________________________________________________________________
+#1. LOADING PACKAGES ####
 
 library(tidyverse)
 library(BioTIMEr)
@@ -8,17 +8,34 @@ library(terra)
 library(predictsFunctions)
 
 
-#_______________________________________________________________________
-####  Importing datasets ####
 
-#BioTIME
+#___________________________________________________________________________
+#2. ACCESSING DATABASES ####
+
+
+#___________________________________________________________________________
+####  BioTIME ####
+
 biotime = read.csv("datasets/BioTIMEQuery_24_06_2021.csv")
 
 str(biotime)
 summary(biotime)
 
+max(biotime$YEAR)
+hist(biotime$YEAR)
 
-#worldclim
+resurvey_2020 <- biotime %>%
+  filter(YEAR >= 2010)
+
+hist(resurvey_2020$YEAR)
+
+# read more about structure!
+
+
+
+#___________________________________________________________________________
+#### WorldClim ####
+
 wc_data <- worldclim_global(var = "bio", res = 10, path = "./datasets") # downloading BioClim variables at 10-minute resolution
 
 bio1 <- wc_data[[1]] # Loading specific variable (e.g., Bio1 - Annual Mean Temperature)
@@ -26,6 +43,47 @@ bio1 <- wc_data[[1]] # Loading specific variable (e.g., Bio1 - Annual Mean Tempe
 plot(bio1)
 
 
-#PREDICTS
+
+#___________________________________________________________________________
+#### PREDICTS ####
+
 webFile <- url("https://timnewbold.github.io/predicts_database.rds?dl=1")
 predicts <- readRDS(webFile)
+
+str(predicts)
+summary(predicts$Diversity_metric)
+
+
+predicts_SR <- predicts %>%
+  filter(Diversity_metric == "species richness")
+
+ggplot(predicts_SR, aes(x = Measurement)) +
+  geom_histogram(fill = "darkgreen",
+                 color = "black") +
+  labs(x = "Measurement",
+       y = "Frequency") +
+  theme_minimal()
+
+
+predicts_abundance <- predicts %>%
+  filter(Diversity_metric == "abundance")
+
+ggplot(predicts_abundance, aes(x = Measurement)) +
+  geom_histogram(fill = "darkgreen",
+                 color = "black") +
+  labs(x = "Measurement",
+       y = "Frequency") +
+  theme_minimal()
+
+
+
+#___________________________________________________________________________
+#### sREplot ####
+
+
+
+
+#___________________________________________________________________________
+#### Breeding Bird Survey ####
+
+
